d2698f8e3c487134d72b6fd5d1f38f2f727433fd
satheshsat <satheshs.sat@gmail.com>
2021-06-16 11:35:16 +0530

    added functionality quantity button

--- src/app/product/product.page.ts
+++ src/app/product/product.page.ts
@@ -3,21 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.page.html',
-  styleUrls: ['./product.page.scss'],
+  styleUrls: ['./product.page.scss']
 })
 export class ProductPage implements OnInit {
-
   product = {
-    name: "Shirts",
-    img: "assets/products/Shirts.jpg",
-    price: "$ 100",
-    content: "Keep close to Nature's heart... and break clear away, once in awhile,"+
-    "and climb a mountain or spend a week in the woods. Wash your spirit clean."
+    name: 'Shirts',
+    img: 'assets/products/Shirts.jpg',
+    price: '$ 100',
+    content:
+      "Keep close to Nature's heart... and break clear away, once in awhile," +
+      'and climb a mountain or spend a week in the woods. Wash your spirit clean.'
   };
 
-  constructor() { }
+  quantity = 1;
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
